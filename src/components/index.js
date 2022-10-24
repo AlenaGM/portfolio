4e67b2a0a84370e4d25b10faabcec4149a7f8b05
AlenaGM <alena.guillaume4@gmail.com>
2022-10-24 15:32:42 +0200

    setup basic structure

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import ErrorMessage from "./errors/ErrorMessage";
+import Footer from "./footer/Footer";
+import Home from "./home/Home";
+import NavBar from "./navBar/NavBar";
+import Projects from "./projects/Projects";
+import SingleProject from "./singleProject/SingleProject";
+import Skills from "./skills/Skills";
+import Spinner from "./spinner/Spinner";
+
+export {
+  About,
+  Contact,
+  ErrorMessage,
+  Footer,
+  Home,
+  NavBar,
+  Projects,
+  SingleProject,
+  Skills,
+  Spinner,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import SkillsPage from "./SkillsPage";
+import ProjectsPage from "./ProjectsPage";
+import ContactPage from "./ContactPage";
+import Page404 from "./Page404";
+
+export { HomePage, AboutPage, SkillsPage, ProjectsPage, ContactPage, Page404 };
