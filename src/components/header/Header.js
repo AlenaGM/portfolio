17739fc09cf8bcfcd7e8054f8a62a931f34ebfe0
AlenaGM <alena.guillaume4@gmail.com>
2022-10-26 11:59:23 +0200

    added hamburger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 import logo from "../../assets/images/logo.png";
 
@@ -19,8 +20,8 @@ const Header = () => {
           </h1>
         </div>
       </Link>
-      <nav className="app__header_menu">
-        <ul>
+      <nav className="app__navbar">
+        <ul className="app__navbar_links">
           {["home", "about", "skills", "projects", "contact"].map((item) => (
             <li key={item}>
               <NavLink
@@ -44,6 +45,45 @@ const Header = () => {
             </a>
           </li>
         </ul>
+        <div className="app__navbar_menu">
+          <p onClick={() => setMenuOpen(true)}>Open</p>
+          {menuOpen && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: "easeOut" }}
+            >
+              <p onClick={() => setMenuOpen(false)}>Close</p>
+              <ul>
+                {["home", "about", "skills", "projects", "contact"].map(
+                  (item) => (
+                    <li key={item}>
+                      <NavLink
+                        end
+                        style={({ isActive }) => ({
+                          color: isActive ? "#3ABDB5" : "inherit",
+                        })}
+                        to={`/${item}`}
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        {item}
+                      </NavLink>
+                    </li>
+                  )
+                )}
+                <li className="app__header_resume button" type="button">
+                  <a
+                    href="https://github.com/AlenaGM"
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </motion.div>
+          )}
+        </div>
       </nav>
       <div className="app__header_social">
         <SocialIcon
